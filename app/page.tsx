5d0cdf3a515c76adc7aa150c0b6e51a745bc7de6
Rattin Sadman <rattin99m18@gmail.com>
2024-04-27 20:57:17 +0600

    ui change and navbar scrollsection

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,18 @@
 "use client";
 
-import { useState } from "react";
+import Navbar from "@/components/ui/Navbar";
+import { useEffect, useState } from "react";
 import { AudioRecorder } from "react-audio-voice-recorder";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
+import {Menu} from 'lucide-react';
+
 
 export default function Home() {
 
-  const [text,setText] = useState('')
+  const [text,setText] = useState('');
+  const [summary,setSummary] = useState('');
+  const [notes,setNotes] = useState('');
+  const [isNavbarOpen, setNavbarOpen] = useState(false);
 
 
   async function sendFile(formData: FormData) {
@@ -20,12 +27,10 @@ export default function Home() {
     }
 
     const res = await response.json();
-
-    console.log(res)
     setText(res.text)
   }
 
-  const addAudioELement = async (blob:Blob) => {
+  const handleAudioRecord = async (blob:Blob) => {
    const fileReader = new FileReader();
    fileReader.readAsArrayBuffer(blob);
    fileReader.onload = async () => {
@@ -38,14 +43,96 @@ export default function Home() {
    }
   }
 
-  return (
-    <main>
-      <AudioRecorder onRecordingComplete={addAudioELement} audioTrackConstraints={{
-        noiseSuppression: true,
-        echoCancellation: true
-      }} onNotAllowedOrFound={(err) => {console.log(err)}} downloadOnSavePress = {false} downloadFileExtension="mp3" />
+  const getTranscriptionSummary = async (text: string) => {
+    const response = await fetch('http://localhost:5000/summary',{
+      method: "POST",
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify({
+        "text": text
+      }),
+    })
 
-      <p>{text}</p>
+    if(!response.ok) {
+      console.log('Error:',response);
+    }
+
+    const res = await response.json();
+
+    setSummary(res.choices[0].message.content)
+  }
+
+  const getTranscriptionNotes = async (text: string) => {
+    const response = await fetch('http://localhost:5000/notes',{
+      method: "POST",
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify({
+        "text": text
+      }),
+    })
+
+    if(!response.ok) {
+      console.log('Error:',response);
+    }
+
+    const res = await response.json();
+
+    setNotes(res.choices[0].message.content)
+  }
+
+  useEffect( () =>{
+   if( text !== ""){
+    getTranscriptionSummary(text)
+    getTranscriptionNotes(text)
+   }
+  },[text])
+
+  return (
+    <main className="w-screen h-screen flex">
+      <Menu onClick={ () => {setNavbarOpen(!isNavbarOpen)} } className="size-10 m-2 block absolute top-0 right-0 sm:hidden" />
+      <Navbar isOpen= {isNavbarOpen}/>
+     
+     <div className="w-full bg-muted   sm:w-[88%] h-screen flex flex-col justify-center items-center">
+        <div className="w-1/3 h-1/5 flex flex-col justify-around items-center">
+          <div className="flex flex-col justify-center items-center mt-5">
+            <div className="p-2">
+              <input type="text" className="text-center bg-muted text-2xl"  defaultValue={'Meeting'}/>
+            </div>
+            <span >April 25, 2024</span>
+          </div>
+          <div className="p-2 mt-2">
+            <AudioRecorder onRecordingComplete={handleAudioRecord}        audioTrackConstraints={{
+                noiseSuppression: true,
+                echoCancellation: true
+                }} onNotAllowedOrFound={(err) => {console.log(err)}} downloadOnSavePress = {false} downloadFileExtension="mp3" />
+          </div>
+        </div>
+        <div className=" w-full bg-white h-3/4 flex justify-center">
+          <Tabs className="w-full "  defaultValue="transcribe" >
+              <TabsList className="flex justify-center items-center bg-muted rounded-none">
+                <TabsTrigger value="transcribe">Transcribe</TabsTrigger>
+                <TabsTrigger value="summary">Summary</TabsTrigger>
+                <TabsTrigger value="notes" >Notes</TabsTrigger>
+              </TabsList>
+             <div className="w-full flex justify-center ">
+               <div className="w-full sm:w-1/2">
+                <TabsContent value="transcribe" className=" flex justify-center">
+                    <p className="text-wrap p-2 text-center">
+                      {text}
+                    </p>
+                  </TabsContent>
+                  <TabsContent value="summary" className=" flex justify-center"> <p className="text-wrap p-2 text-center">
+                      {summary}
+                    </p></TabsContent>
+                  <TabsContent value="notes" className=" flex justify-center"><p className="text-wrap p-2 text-center">
+                      {notes}
+                    </p>
+                  </TabsContent>
+               </div>
+             </div>
+          </Tabs>
+        </div>
+     </div>
+        
     </main>
   );
 }
---
+++ components/ui/Navbar.tsx
@@ -0,0 +1,89 @@
+import { Button } from "./button";
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { LogOut,Settings } from "lucide-react";
+import { ScrollArea } from "@/components/ui/scroll-area"
+
+
+  type Props = {
+    isOpen: boolean
+  }
+
+  const meetings = [
+    {title:"meeting"},
+    {title:"transcription"},
+    {title:"date"},
+    {title:"meeting"},
+    {title:"transcription"},
+    {title:"date"}, {title:"meeting"},
+    {title:"transcription"},
+    {title:"date"},
+    {title:"meeting"},
+    {title:"transcription"},
+    {title:"date"},
+    {title:"meeting"},
+    {title:"transcription"},
+    {title:"date"}, {title:"meeting"},
+    {title:"transcription"},
+    {title:"date"},
+    {title:"meeting"},
+    {title:"transcription"},
+    {title:"date"},
+    {title:"meeting"},
+    {title:"transcription"},
+    {title:"date"}, {title:"meeting"},
+    {title:"transcription"},
+    {title:"date"},
+    {title:"date"},
+
+
+  ]
+
+
+
+const Navbar: React.FC<Props> = ({isOpen}) => {
+
+    return (
+        <nav  className={`${
+            isOpen ? "flex fixed inset-x-0 bottom-0 sm:static" : "hidden sm:flex"
+          } h-1/2 sm:h-screen w-full sm:w-[250px] flex-col justify-between p-2 bg-white border-r shadow-sm`}>
+            <Button className=""> + New Meeting</Button>
+            <ScrollArea className="h-5/6">
+                {
+                    meetings.map((value,index) => (
+                        <div className="p-2 mr-3 cursor-pointer hover:bg-muted rounded-md">
+                            <span>{value.title}</span>
+                        </div>
+                    ))
+                }
+            </ScrollArea>
+            <DropdownMenu>
+                <DropdownMenuTrigger className="w-full">
+                    <div className="flex items-center justify-center  px-4 py-2 bg-gray-100 rounded-md">
+                    <Avatar className="mr-2 rounded-full">
+                        <AvatarImage src="./" />
+                        <AvatarFallback className="p-2 bg-red-400">RS</AvatarFallback>
+                    </Avatar>
+                    <span className="text-3xl sm:text-lg">Rattin Sadman</span>
+                    </div>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent className=" min-w-56">
+                    <DropdownMenuItem>rattin99m8@gmail.com</DropdownMenuItem>
+                    <DropdownMenuSeparator/>
+                    <DropdownMenuItem> <Settings className="size-4 mr-2"/> <span>settings</span></DropdownMenuItem>
+                    <DropdownMenuItem> <LogOut className="size-4 mr-2" /> log out</DropdownMenuItem>
+                </DropdownMenuContent>
+            </DropdownMenu>
+        </nav>
+    )
+}
+
+
+export default Navbar;
